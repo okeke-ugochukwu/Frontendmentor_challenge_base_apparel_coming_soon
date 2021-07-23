0e516b836d47c75b38c71bfafe231b2b782cee87
okekeugochukwu.dev@gmail.com <okekeugochukwu.dev@gmail.com>
2021-07-23 12:34:22 +0100

    Landmark, going to research on email validation and regex

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-var test_Text = "Code works at this point"
-document.getElementById("submit").addEventListener("click", function(e) {
-    e.preventDefault();
+    var test_Text = "Code works at this point";
 
-    var userEmail = document.getElementById("email").innerText;
-    console.log(test_Text);
 
-    if (userEmail == " ") {
-       alert(userEmail)
-    }
-})
+    document.getElementById("submit").addEventListener("click", function(e) {
+        e.preventDefault();
+        
+        emailFormat =  /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+
+        var userEmail = document.getElementById("email").value;
+
+        if (userEmail == "") {
+        document.getElementById("mssg").innerText = "Please provide an email";
+        }
+        else if (userEmail.match(emailFormat)) {
+            document.getElementById("form").submit();
+        }
+
+    })
+
+    document.getElementById("email").addEventListener("keyup", function () {
+        document.getElementById("mssg").innerText = "";
+    })
