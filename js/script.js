72742bff074704ce8a946fdb0f4d54845a49504d
okekeugochukwu.dev@gmail.com <okekeugochukwu.dev@gmail.com>
2021-07-24 04:14:39 +0100

    Error interations added

--- js/script.js
+++ js/script.js
@@ -7,25 +7,31 @@
     document.getElementById("submit").addEventListener("click", function(e) {
         e.preventDefault();
 
-        emailFormat =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        emailFormat =
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            //courtsey of edureka.co ;)
+
         var userEmail = emailInput.value;
 
         if (userEmail == "") {
-            errorMssgBox.innerText = "Please provide an email";
             emailInput.classList.add("error");
+            errorMssgBox.innerText = "Please provide an email";
+            document.getElementById("error-icon").style.display = "block";
             emailInput.focus();
         }
         else if (userEmail.match(emailFormat)) {
             alert("Yay, you've joined the mailing list");
         }
         else {
-            errorMssgBox.innerText = "Please provide a valid email";
             emailInput.classList.add("error");
+            errorMssgBox.innerText = "Please provide a valid email";
+            document.getElementById("error-icon").style.display = "block";
             emailInput.focus();
         }
     })
 
     document.getElementById("email").addEventListener("keyup", function () {
         document.getElementById("mssg").innerText = "";
-        emailInput.classList.remove("error");
+            document.getElementById("error-icon").style.display = "none";
+            emailInput.classList.remove("error");
     })
