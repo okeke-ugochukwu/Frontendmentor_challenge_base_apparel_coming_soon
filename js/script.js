4a143a5439714a29b6f0743a03379d96876439be
okekeugochukwu.dev@gmail.com <okekeugochukwu.dev@gmail.com>
2021-07-24 03:27:33 +0100

    Email validation algorithm done, will still properly learn regex

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
-    var test_Text = "Code works at this point";
+    var test = "Code works at this point";
 
+    var emailInput = document.getElementById("email");
+    var submitBtn = document.getElementById("submit");
+    var errorMssgBox = document.getElementById("mssg");
 
     document.getElementById("submit").addEventListener("click", function(e) {
         e.preventDefault();
-        
-        emailFormat =  /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
 
-        var userEmail = document.getElementById("email").value;
+        emailFormat =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        var userEmail = emailInput.value;
 
         if (userEmail == "") {
-        document.getElementById("mssg").innerText = "Please provide an email";
+            errorMssgBox.innerText = "Please provide an email";
+            emailInput.classList.add("error");
+            emailInput.focus();
         }
         else if (userEmail.match(emailFormat)) {
-            document.getElementById("form").submit();
+            alert("Yay, you've joined the mailing list");
         }
-
+        else {
+            errorMssgBox.innerText = "Please provide a valid email";
+            emailInput.classList.add("error");
+            emailInput.focus();
+        }
     })
 
     document.getElementById("email").addEventListener("keyup", function () {
         document.getElementById("mssg").innerText = "";
+        emailInput.classList.remove("error");
     })
